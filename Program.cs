ffb05246a3528b51d15995df044e210413c66d04
maktumpasha <106083369+maktumpasha@users.noreply.github.com>
2025-03-02 05:00:22 +0530

    Comiting the new line to master

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello from Maktum");
 Console.WriteLine("Hello from Maktum commit 2");
 Console.WriteLine("Hello from Maktum commit 3");
-console.WriteLine("added this from new branch "Feature_Branch1");
+Console.WriteLine("added this from new branch "+"Feature_Branch1");
 
-console.WriteLine("squash and merge --- commit will be added to base branch");
-console.WriteLine("squash and merge --- commit will be added to base branch new line");
+Console.WriteLine("squash and merge --- commit will be added to base branch");
+Console.WriteLine("squash and merge --- commit will be added to base branch new line");
+Console.WriteLine("added new line in master");
